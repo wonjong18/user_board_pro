99809046292373735d7384d32a0bf844c8d4a067
wonjong18 <wonjong18@gmail.com>
2022-02-07 18:23:40 +0900

    user_board_pro

---
+++ user_board_pro/src/user/join.py
@@ -0,0 +1,11 @@
+#-*- coding : utf-8 -*-
+import logging
+import traceback
+logging.basicConfig(level=logging.ERROR)
+
+from flask import request
+from flask_restx import Resource, Namespace, reqparse
+import datetime
+
+
+join = Namespace('join')
